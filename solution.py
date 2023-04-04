0bf90e8b18e65b9ceb346744587cbeec57fab500
artist_mary <94290722+BeautifulSnowleopard@users.noreply.github.com>
2023-04-04 19:41:04 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm, chi2
-
+from scipy.stats import norm
+from scipy.stats import chi2
 
 chat_id = 1897874711
 
 def solution(p: float, x: np.array) -> tuple:
-    n = len(x)
-    chi_sq = chi2.ppf(p, df=2*n)
-    s = sum([d**2 for d in x])/n
-    left = s*2*n/chi_sq
-    right = s*2*n/chi2.ppf(1-p, df=2*n)
-    return (left, right)
+    alpha = 1 - p
+    size = len(x)
+    left = np.sqrt(size * (x ** 2).mean() / (58 * chi2.ppf(q=1 - alpha / 2, df=2 * size)))
+    right = np.sqrt(size * (x ** 2).mean() / (58 * chi2.ppf(q=alpha / 2, df=2 * size)))
+    
+    return left, \
+           right

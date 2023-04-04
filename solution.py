57fac3a5ad9dd4d5055691e15bd55388929b4935
artist_mary <94290722+BeautifulSnowleopard@users.noreply.github.com>
2023-04-04 16:39:16 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import norm, chi2
 
 
 chat_id = 1897874711
@@ -9,6 +9,7 @@ chat_id = 1897874711
 def solution(p: float, x: np.array) -> tuple:
     # вычисляем степени свободы
     df = len(x) - 1
+
     # вычисляем выборочное среднее и выборочную смещенную оценку дисперсии
     mean = np.mean(x)
     var = np.var(x, ddof=1)
@@ -16,10 +17,5 @@ def solution(p: float, x: np.array) -> tuple:
     # вычисляем левую и правую границы доверительного интервала
     left = np.sqrt(df * var / chi2.ppf(1 - p/2, df))
     right = np.sqrt(df * var / chi2.ppf(p/2, df))
-
+    
     return (mean - left, mean + right)
-
-
-
-
-
